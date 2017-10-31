02f0dd9f9943a15f684648c8502e5022c18e800a
Rocky Signavong <rsignavong@gmail.com>
2017-10-31 21:02:37 +0100

    first commit

---
+++ install/brew.sh
@@ -0,0 +1,60 @@
+if ! is-macos -o ! is-executable ruby -o ! is-executable curl -o ! is-executable git; then
+  echo "Skipped: Homebrew (missing: ruby, curl and/or git)"
+  return
+fi
+
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew tap Goles/battery
+brew update
+brew upgrade
+
+# Install packages
+
+apps=(
+  bash-completion2
+  bats
+  battery
+  coreutils
+  crystal-lang
+  diff-so-fancy
+  dockutil
+  erlang
+  elixir
+  ffmpeg
+  fasd
+  fzf
+  gifsicle
+  git
+  git-extras
+  gnu-sed --with-default-names
+  grep --with-default-names
+  hub
+  httpie
+  imagemagick
+  jq
+  lynx
+  mackup
+  nano
+  pandoc
+  psgrep
+  python
+  reattach-to-user-namespace
+  shellcheck
+  ssh-copy-id
+  tig
+  tmux
+  the_silver_searcher
+  tree
+  unar
+  vim --with-override-system-vi
+  wget
+  wifi-password
+)
+
+brew install "${apps[@]}"
+
+export DOTFILES_BREW_PREFIX_COREUTILS=`brew --prefix coreutils`
+set-config "DOTFILES_BREW_PREFIX_COREUTILS" "$DOTFILES_BREW_PREFIX_COREUTILS" "$DOTFILES_CACHE"
+
+ln -sfv "$DOTFILES_DIR/etc/mackup/.mackup.cfg" ~
